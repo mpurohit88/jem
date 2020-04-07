11badee745d911a68fb4c4023b57fd03ea163719
mukesh purohit <mpurohit88@gmail.com>
2020-04-07 20:22:53 +1245

    server changes

--- jem/server/index.js
+++ jem/server/index.js
@@ -3,7 +3,12 @@ const app = express()
 var testAPIRouter = require("./index");
 const port =process.env.PORT || 3001;
 var cors = require("cors");
-app.get('/', (req, res) => res.send('Hello World!'))
+
+const Product = require('./controllers/product')
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.pust('/addProduct', (req, res, next) => { new Product(req, res, next).add()})
+
 
 app.use("/index", testAPIRouter);
 app.use(cors());
